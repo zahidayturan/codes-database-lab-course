2cb469c5c3e4ec133a3f59327808bc83fcfad6e9
Zahid Ayturan <91957947+zahidayturan@users.noreply.github.com>
2025-03-14 22:28:31 +0300

    Update db.sql

--- labwork-2/db.sql
+++ labwork-2/db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE NHL_DB ON PRIMARY 
 ( 
 NAME= NHL_DB, 
-FILENAME = 'D:\DERS\vtys lab\2\NHL_DB_data.mdf', 
+FILENAME = '<path>\NHL_DB_data.mdf', 
 SIZE = 16MB, 
 MAXSIZE = unlimited, 
 FILEGROWTH = 15% 
@@ -9,7 +9,7 @@ FILEGROWTH = 15%
 LOG ON 
 ( 
 NAME= NHL_DB_LOG, 
-FILENAME = 'D:\DERS\vtys lab\2\NHL_DB_LOG.ldf', 
+FILENAME = '<path>\NHL_DB_LOG.ldf', 
 SIZE = 14MB, 
 MAXSIZE = unlimited, 
 FILEGROWTH = 10% 
